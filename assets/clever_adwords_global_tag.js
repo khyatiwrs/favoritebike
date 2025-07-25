32b6086498dd4f0cf73f9fc41ea5788e6226c311
khyatiwrs <97171978+khyatiwrs@users.noreply.github.com>
2025-07-25 12:10:12 +0530

    Add files via upload

---
+++ assets/clever_adwords_global_tag.js
@@ -0,0 +1,10 @@
+$.getScript('https://www.googletagmanager.com/gtag/js?id=AW-1009320767', function(){
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-1009320767');
+
+});
